bb3d6b1db50946ebc31321827b68a46dcfc12849
Chang Liu <liuc223@gmail.com>
2025-05-26 23:33:35 -0400

    Fix resource path for KuzuTests in Package.swift and template

--- Package.swift
+++ Package.swift
@@ -1120,7 +1120,7 @@ let package = Package(
             name: "KuzuTests",
             dependencies: ["Kuzu"],
             resources: [
-                .copy("../Dataset")
+                .copy("Dataset")
             ]
         )
     ],
