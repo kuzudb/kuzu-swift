47310dbb76f4d4af13a44d3e9c01eb8e2c7f9a5a
mewim <14037726+mewim@users.noreply.github.com>
2025-07-02 08:05:05 +0000

    Update Kuzu from branch

--- Sources/cxx-kuzu/kuzu/build/src/extension/codegen/include/generated_extension_loader.h
+++ Sources/cxx-kuzu/kuzu/build/src/extension/codegen/include/generated_extension_loader.h
@@ -10,7 +10,7 @@
 namespace kuzu {
 namespace extension {
 
-void loadLinkedExtensions(main::ClientContext* context);
+void loadLinkedExtensions(main::ClientContext* context, std::vector<LoadedExtension>& loadedExtensions);
 
 } // namespace extension
 } // namespace kuzu
