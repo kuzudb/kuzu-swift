b7161be96bb79b94e26ba5f72a6341588dacd850
mewim <14037726+mewim@users.noreply.github.com>
2025-08-06 21:09:41 +0000

    Update Kuzu from branch master

---
+++ Sources/cxx-kuzu/kuzu/build/src/extension/codegen/include/generated_extension_loader.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "main/client_context.h"
+#include "fts_extension.h"
+#include "json_extension.h"
+#include "vector_extension.h"
+#include "algo_extension.h"
+
+
+namespace kuzu {
+namespace extension {
+
+void loadLinkedExtensions(main::ClientContext* context, std::vector<LoadedExtension>& loadedExtensions);
+
+} // namespace extension
+} // namespace kuzu
